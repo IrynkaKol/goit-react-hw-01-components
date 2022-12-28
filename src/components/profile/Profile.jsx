6baed9ecef6dbd35da348004910c9c16573cf878
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-12-29 00:03:55 +0100

    Update Profile.jsx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,33 +1,34 @@
 
 
-export const Profile = ({ avatar, username, tag, location, stats }) => (
-      <div class="profile">
-  <div class="description">
+const Profile = ({ avatar, username, tag, location, stats }) => (
+      <div className="profile">
+  <div className="description">
     <img
       src={avatar}
       alt={username}
-      class="avatar"
+      className="avatar"
     />
-    <p class="name">{username}</p>
-    <p class="tag">@{tag}</p>
-    <p class="location">{location}</p>
+    <p className="name">{username}</p>
+    <p className="tag">@{tag}</p>
+    <p className="location">{location}</p>
   </div>
 
-  <ul class="stats">
+  <ul className="stats">
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{stats.followers}</span>
+      <span className="label">Followers</span>
+      <span className="quantity">{stats.followers}</span>
     </li>
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">{stats.views}</span>
+      <span className="label">Views</span>
+      <span className="quantity">{stats.views}</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">{stats.likes}</span>
+      <span className="label">Likes</span>
+      <span className="quantity">{stats.likes}</span>
     </li>
   </ul>
 </div>
 )
 
+export default Profile;
 //ReactDOM.createRoot(document.getElementById("root")).render(<Profile />);
