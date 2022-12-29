fd3f517b0056c7b9ad98fdaed02c46376ec3a566
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-12-29 15:09:38 +0100

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,41 +1,38 @@
-import PropTypes from "prop-types";
-
+import PropTypes from 'prop-types';
 
 const Statistics = ({ title, stats }) => (
-    <section className="statistics">
-        {title && (<h2 className="title">{title}</h2>)}
-  
-
-  <ul className="stat-list">
-    <li className="item">
-      <span className="label">{stats[0].label}</span>
-      <span className="percentage">{stats[0].percentage}</span>
-    </li>
-    <li className="item">
-      <span className="label">{stats[1].label}</span>
-      <span className="percentage">{stats[1].percentage}</span>
-    </li>
-    <li className="item">
-      <span className="label">{stats[2].label}</span>
-      <span className="percentage">{stats[2].percentage}</span>
-    </li>
-    <li className="item">
-      <span className="label">{stats[3].label}</span>
-      <span className="percentage">{stats[3].percentage}</span>
-    </li>
-  </ul>
-</section>
+  <section className="statistics">
+    {title && <h2 className="title">{title}</h2>}
+    <ul className="stat-list">
+      <li className="item">
+        <span className="label">{stats[0].label}</span>
+        <span className="percentage">{stats[0].percentage}</span>
+      </li>
+      <li className="item">
+        <span className="label">{stats[1].label}</span>
+        <span className="percentage">{stats[1].percentage}</span>
+      </li>
+      <li className="item">
+        <span className="label">{stats[2].label}</span>
+        <span className="percentage">{stats[2].percentage}</span>
+      </li>
+      <li className="item">
+        <span className="label">{stats[3].label}</span>
+        <span className="percentage">{stats[3].percentage}</span>
+      </li>
+    </ul>
+  </section>
 );
 
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
-        PropTypes.shape({
-id: PropTypes.string,
-label: PropTypes.string,
-percentage: PropTypes.number,
-        })
-    ),
-}
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
 
 export default Statistics;
