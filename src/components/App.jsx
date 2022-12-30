03430151e8775ef9165dea7cf13db64bada89bc2
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-12-30 21:37:00 +0100

    add transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './profile/Profile';
 import Statistics from './statistics/Statistics';
 import FriendList from './friendList/FriendList';
+import TransactionHistory from './transactionHistory/TransactionHistory'
 
 import user from './profile/user.json';
 import data from './statistics/data.json';
 import friends from './friendList/friends.json';
+import transactions from './transactionHistory/transactions.json'
 
 export const App = () => {
   return (
@@ -31,6 +33,8 @@ export const App = () => {
       <Statistics stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />; 
     </div>
   );
 };
